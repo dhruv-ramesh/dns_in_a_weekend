dc989307d46c7c18bd6d31b0c02139d185f45660
Dhruv Ramesh <dhruvramesh1@gmail.com>
2024-03-31 17:34:04 -0400

    Implemented DNS response parser

---
+++ dns_parser.py
@@ -0,0 +1,78 @@
+from dns_query import DNSQuestion, DNSHeader, build_query
+from dataclasses import dataclass
+import struct
+from typing import List
+
+
+@dataclass
+class DNSRecord:
+    name: bytes
+    type_: int
+    class_: int
+    ttl: int
+    data: bytes
+
+
+@dataclass
+class DNSPacket:
+    header: DNSHeader
+    questions: List[DNSQuestion]
+    answers: List[DNSRecord]
+    authorities: List[DNSRecord]
+    additionals: List[DNSRecord]
+
+
+def parse_header(reader):
+    items = struct.unpack("!HHHHHH", reader.read(12))
+    return DNSHeader(*items)
+
+
+def decode_compressed_name(length, reader):
+    pointer_bytes = bytes([length & 0b0011_1111]) + reader.read(1)
+    pointer = struct.unpack("!H", pointer_bytes)[0]
+    current_pos = reader.tell()
+    reader.seek(pointer)
+    result = decode_name(reader)
+    reader.seek(current_pos)
+    return result
+
+
+def decode_name(reader):
+    parts = []
+    while (length := reader.read(1)[0]) != 0:
+        if length & 0b1100_0000:
+            parts.append(decode_compressed_name(length, reader))
+            break
+        else:
+            parts.append(reader.read(length))
+    return b".".join(parts)
+
+
+def parse_question(reader):
+    name = decode_name(reader)
+    data = reader.read(4)
+    type_, class_ = struct.unpack("!HH", data)
+    return DNSQuestion(name, type_, class_)
+
+
+def parse_record(reader):
+    name = decode_name(reader)
+    data = reader.read(10)
+    type_, class_, ttl, data_len = struct.unpack("!HHIH", data)
+    data = reader.read(data_len)
+    return DNSRecord(name, type_, class_, ttl, data)
+
+
+def parse_dns_packet(data):
+    reader = BytesIO(data)
+    header = parse_header(reader)
+    questions = [parse_question(reader) for _ in range(header.num_questions)]
+    answers = [parse_record(reader) for _ in range(header.num_answers)]
+    authorities = [parse_record(reader) for _ in range(header.num_authorities)]
+    additionals = [parse_record(reader) for _ in range(header.num_additionals)]
+
+    return DNSPacket(header, questions, answers, authorities, additionals)
+
+
+def ip_to_string(ip):
+    return ".".join(str(i) for i in ip)
